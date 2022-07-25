f3e91cb652cef3ead6f68f867fff803873313c77
DiantArts <DiantArts@gmail.com>
2022-07-25 10:49:52 +0200

    Removing unused code

--- Shaders/Vertex/simple.glsl
+++ Shaders/Vertex/simple.glsl
@@ -20,9 +20,6 @@ void main()
 {
     gl_Position = push.tranform * vec4(position, 1.0f);
 
-    // mat3 normalMatrix = transpose(inverse(mat3(push.modelMatrix)));
-    // vec3 normalWorldSpace = normalize(normalMatrix * normal);
-
     vec3 normalWorldSpace = normalize(mat3(push.normalMatrix) * normal);
     float lightIntensity = AMBIENT + max(dot(normalWorldSpace, DIRECTION_TO_LIGHT), 0);
 
